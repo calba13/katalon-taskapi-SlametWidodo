417f12c89727fc9c97a3a1dd72a84d29ae88bcde
calba13 <wiwied.qc1@gmail.com>
2023-12-28 21:08:34 +0700

    update

--- Scripts/TC-USERS/00. GET List User/Script1703583928174.groovy
+++
@@ -1,6 +0,0 @@
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-
-'STEP : Running API GET List USERS'
-CustomKeywords.'finalProjectKW.apiUserFeature.getListUsers'(findTestObject('Object Repository/REST-API USERS/GET - List Users'))
-
-
